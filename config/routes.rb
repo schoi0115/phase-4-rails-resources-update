ba32971406b664accf665586ac1a86943d7c7a0a
Seungil <schoi0115@gmail.com>
2021-08-30 21:42:05 -0400

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  resources :birds, only: [:index, :show, :create, :update, :destroy]
   patch "/birds/:id/like", to: "birds#increment_likes"
   patch "/birds/:id/dislike", to: "birds#decrement_likes"
+  
 end
